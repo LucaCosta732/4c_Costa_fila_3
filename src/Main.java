ebe189b63949f0c02cbf398219ce4fba2f6938fe
4ci <4ci@INF13>
2022-04-27 12:36:34 +0200

    versione margari 2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("margari");
+		System.out.println("margari 2");
 		System.out.println("CAUSO MATTIA");
 		
 		
